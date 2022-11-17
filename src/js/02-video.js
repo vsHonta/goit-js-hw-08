929775cfc56121cae34b54990eb809e1671d7812
Viktor H <66110545+vsHonta@users.noreply.github.com>
2022-11-17 17:50:36 -0600

    3rd done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
 
+// player.on('play', function () {
+// console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+// console.log('title:', title);
+// });
+
+import Player from '@vimeo/player';
+
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+const form = document.querySelector('form');
+const inputEmail = document.querySelector("input");
+const textArea = document.querySelector('textarea');
+const button = document.querySelector('button')
+let obj = {
+        email: '',
+        message: ''
+    }
+
+var throttle = require('lodash.throttle');
+
+
+try {
+    const parsedObj = JSON.parse(localStorage.getItem('feedback-form-state'))
+    inputEmail.value = parsedObj.email
+    textArea.textContent = parsedObj.message 
+} catch (error) {}
+
+
+
+const onInput = evt => {
+    if (evt.target.type === 'email') {
+        obj.email = evt.target.value
+    } else {obj.message = evt.target.value}
+    
+    localStorage.setItem('feedback-form-state', JSON.stringify(obj))
+}
+
+const onSubmit = evt => {
+    evt.preventDefault()
+    console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+    localStorage.clear()
+
+    inputEmail.value = ''
+    textArea.textContent = ''
+}
+
+
+
+form.addEventListener('submit', onSubmit)
+
+form.addEventListener('input', throttle(onInput, 500))
+
+
+
+
+
+
 
